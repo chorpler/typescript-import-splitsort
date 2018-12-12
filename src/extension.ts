3bd74f78518011afe5eaea369cde01bface21257
David Sargeant <chorpler@gmail.com>
2018-12-12 16:51:00 -0600

    Latest stuff
    Yep

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,107 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+// 'use strict';
+// // The module 'vscode' contains the VS Code extensibility API
+// // Import the module and reference it with the alias vscode in your code below
+// import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+// // this method is called when your extension is activated
+// // your extension is activated the very first time the command is executed
+// export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "typescript-import-splitsort" is now active!');
+//     // Use the console to output diagnostic information (console.log) and errors (console.error)
+//     // This line of code will only be executed once when your extension is activated
+//     console.log('Congratulations, your extension "typescript-import-splitsort" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+//     // The command has been defined in the package.json file
+//     // Now provide the implementation of the command with  registerCommand
+//     // The commandId parameter must match the command field in package.json
+//     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+//         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
+//         // Display a message box to the user
+//         vscode.window.showInformationMessage('Hello World!');
+//     });
 
-    context.subscriptions.push(disposable);
+//     context.subscriptions.push(disposable);
+// }
+
+// // this method is called when your extension is deactivated
+// export function deactivate() {
+// }
+
+import { Disposable } from 'vscode';
+import { ExtensionContext } from 'vscode';
+import { Parser } from './parser';
+import { TextDocumentWillSaveEvent } from 'vscode';
+import { TextEdit } from 'vscode';
+
+import { commands } from 'vscode';
+import { window } from 'vscode';
+import { workspace } from 'vscode';
+
+/**
+ * import-splitnsort contract
+ */
+
+export function activate(context:ExtensionContext) {
+  const disposable1 = commands.registerCommand('extension.import-splitnsort.splitnsort', splitAndSort);
+  const disposable2 = commands.registerCommand('extension.import-splitnsort.splitonly', splitAndSort);
+  context.subscriptions.push(disposable1);
+  context.subscriptions.push(disposable2);
+  onSave();
+  workspace.onDidChangeConfiguration(() => onSave());
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+export function deactivate() { }
+
+// private functions
+
+// let subscription: Nullable<Disposable>;
+let subscription:Disposable;
+
+function onSave() {
+  const config = workspace.getConfiguration('import-splitnsort');
+  if(config.get<boolean>('on-save')) {
+    if(!subscription) {
+        subscription = workspace.onWillSaveTextDocument(splitAndSortOnSave);
+    }
+  }
+  else {
+    if(subscription) {
+      subscription.dispose();
+      subscription = null;
+    }
+  }
+}
+
+// @see https://github.com/mflorence99/import-splitnsort/issues/2
+
+function splitAndSort() {
+  const editor = window.activeTextEditor;
+  let sortIt = true;
+  const config = workspace.getConfiguration('import-splitnsort');
+  if(config.get<boolean>('leave-unsorted')) {
+    sortIt = false;
+  }
+
+  if(editor.document.languageId === 'typescript') {
+    Parser.makeEdits(editor.document.getText(), sortIt)
+      .then((edits:TextEdit[]) => {
+        if(edits.length > 0) {
+          const range = edits[0].range;
+          const imports = edits[0].newText;
+          editor.edit(edit => edit.replace(range, imports));
+        }
+      });
+  }
+}
+
+function splitAndSortOnSave(event: TextDocumentWillSaveEvent) {
+  let sortIt = true;
+  const config = workspace.getConfiguration('import-splitnsort');
+  if(config.get<boolean>('leave-unsorted')) {
+    sortIt = false;
+  }
+  if(event.document.languageId === 'typescript') {
+    event.waitUntil(Parser.makeEdits(event.document.getText(), sortIt));
+  }
 }
---
+++ src/parser.ts
@@ -0,0 +1,302 @@
+import { ExternalModuleImport } from 'typescript-parser';
+import { File } from 'typescript-parser';
+import { Import } from 'typescript-parser';
+import { NamedImport } from 'typescript-parser';
+import { NamespaceImport } from 'typescript-parser';
+import { Position } from 'vscode';
+import { Range } from 'vscode';
+import { StringImport } from 'typescript-parser';
+import { SymbolSpecifier } from 'typescript-parser';
+import { TextEdit } from 'vscode';
+import { TypescriptParser } from 'typescript-parser';
+
+// const clone:Function = (val:any):any => {
+//   let tmpVal = JSON.stringify(val);
+//   let newVal = JSON.parse(tmpVal);
+//   return newVal;
+// };
+
+/**
+ * Parse TypeScript and extract imports
+ */
+
+export class Parser {
+
+  public defaultImports: Dictionary = { };
+  public externalImports: Dictionary = { };
+  public namedClassImports: Dictionary = { };
+  public namedFunctionImports: Dictionary = { };
+  public namespaceImports: Dictionary = { };
+  public stringImports: string[] = [];
+
+  public range = new Range(0, 0, 0, 0);
+
+  public braces = ['{ ', ' }'];
+  public quote = `'`;
+  public semicolon = ';';
+
+  private parser = new TypescriptParser();
+
+  public static sortImports = false;
+
+  public get sortImports():boolean { return Parser.sortImports; }
+  public set sortImports(val:boolean) { Parser.sortImports = Boolean(val); }
+
+  /** ctor */
+  constructor(private src: string) { }
+
+  /** Make an edit to replace imports */
+  static makeEdits(src: string, toSort:boolean): Promise<TextEdit[]> {
+    return new Promise((resolve, reject) => {
+      if(toSort) {
+        Parser.sortImports = true;
+      }
+      const parser = new Parser(src);
+      parser.parse().then(() => {
+        const imports = parser.produce();
+        resolve((imports.length > 0)? [TextEdit.replace(parser.range, imports)] : []);
+      });
+    });
+  }
+
+  /** Parse the source, extracting the imports */
+  parse(): Promise<any> {
+    if (!this.src.includes('import '))
+      return Promise.resolve();
+    else return new Promise((resolve, reject) => {
+      this.parser.parseSource(this.src).then((file: File) => {
+        let start = Number.MAX_SAFE_INTEGER;
+        let end = Number.MIN_SAFE_INTEGER;
+        file.imports.forEach((node: Import) => {
+          start = Math.min(start, node.start);
+          end = Math.max(end, node.end);
+          // process each type of import
+          switch (node.constructor) {
+            case ExternalModuleImport:
+              this.extractExternal(<ExternalModuleImport>node);
+              break;
+            case NamedImport:
+              this.extractNamed(<NamedImport>node);
+              break;
+            case NamespaceImport:
+              this.extractNamespace(<NamespaceImport>node);
+              break;
+            case StringImport:
+              this.extractString(<StringImport>node);
+              break;
+          }
+        });
+        this.detectCodingStyle(start, end);
+        this.makeRange(start, end);
+        resolve();
+      });
+    });
+  }
+
+  /** Produce sorted import statements */
+  produce(): string {
+    const stmts = this.produceNamespace()
+      .concat(this.produceNamedClass())
+      .concat(this.produceNamedFunction())
+      .concat(this.produceDefault())
+      .concat(this.produceExternal())
+      .concat(this.produceString());
+    // NOTE: if there are any imports at all, there'll be an initial blank line
+    return (stmts.length > 0)? stmts.slice(1).join('\n') : '';
+  }
+
+  // private methods
+
+  private detectCodingStyle(start: number,
+                            end: number): void {
+    const imports = this.src.substring(start, end);
+    if (!imports.includes(this.braces[0]))
+      this.braces[0] = '}';
+    if (!imports.includes(this.braces[1]))
+      this.braces[1] = '}';
+    if (!imports.includes(this.quote))
+      this.quote = '"';
+    if (!imports.includes(this.semicolon))
+      this.semicolon = '';
+  }
+
+  private extractExternal(node: ExternalModuleImport): void {
+    this.externalImports[node.alias] = node.libraryName;
+  }
+
+  private extractNamed(node: NamedImport): void {
+    if (node.defaultAlias)
+      this.defaultImports[node.defaultAlias] = node.libraryName;
+    node.specifiers.forEach((specifier: SymbolSpecifier) => {
+      const initialChar = specifier.specifier[0];
+      const dict = (initialChar === initialChar.toLowerCase())?
+        this.namedFunctionImports : this.namedClassImports;
+      if (specifier.alias)
+        dict[`${specifier.specifier} as ${specifier.alias}`] = node.libraryName;
+      else dict[specifier.specifier] = node.libraryName;
+    });
+  }
+
+  private extractNamespace(node: NamespaceImport): void {
+    this.namespaceImports[node.alias] = node.libraryName;
+  }
+
+  private extractString(node: StringImport): void {
+    this.stringImports.push(node.libraryName);
+  }
+
+  // NOTE: awkward! we have to convert characters from parser into line/character
+  private makeRange(start: number,
+                    end: number): void {
+    let line = 0, character = 0;
+    let spos: Position, epos: Position;
+    for (let ix = 0; ix < end; ix++) {
+      if (ix === start)
+        spos = new Position(line, character);
+      // increment counters
+      if (this.src[ix] === '\n') {
+        line += 1;
+        character = 0;
+      }
+      else character += 1;
+    }
+    // complete range
+    epos = new Position(line, character);
+    this.range = new Range(spos, epos);
+  }
+
+  private produceDefault(): string[] {
+    const stmts: string[] = [];
+    let imports:string[];
+    const value:Dictionary = this.defaultImports;
+    if(this.sortImports) {
+      imports = this.sortedNamesIn(value);
+    } else {
+      imports = this.unsortedNamesIn(value);
+    }
+    imports.forEach((name, ix) => {
+      if (ix === 0)
+        stmts.push('');
+      const library = value[name];
+      stmts.push(`import ${name} from ${this.quote}${library}${this.quote}${this.semicolon}`);
+    });
+    return stmts;
+  }
+
+  private produceExternal(): string[] {
+    const stmts: string[] = [];
+    let imports:string[];
+    const value:Dictionary = this.externalImports;
+    if(this.sortImports) {
+      imports = this.sortedNamesIn(value);
+    } else {
+      imports = this.unsortedNamesIn(value);
+    }
+    imports.forEach((name, ix) => {
+      if (ix === 0)
+        stmts.push('');
+      const library = value[name];
+      stmts.push(`import ${name} = require(${this.quote}${library}${this.quote})${this.semicolon}`);
+    });
+    return stmts;
+  }
+
+  private produceNamedClass(): string[] {
+    const stmts: string[] = [];
+    let imports:string[];
+    const value:Dictionary = this.namedClassImports;
+    if(this.sortImports) {
+      imports = this.sortedNamesIn(value);
+    } else {
+      imports = this.unsortedNamesIn(value);
+    }
+    imports.forEach((name, ix) => {
+      if (ix === 0)
+        stmts.push('');
+      const library = value[name];
+      stmts.push(`import ${this.braces[0]}${name}${this.braces[1]} from ${this.quote}${library}${this.quote}${this.semicolon}`);
+    });
+    return stmts;
+  }
+
+  private produceNamedFunction(): string[] {
+    const stmts: string[] = [];
+    let imports:string[];
+    const value:Dictionary = this.namedFunctionImports;
+    if(this.sortImports) {
+      imports = this.sortedNamesIn(value);
+    } else {
+      imports = this.unsortedNamesIn(value);
+    }
+    imports.forEach((name, ix) => {
+      if (ix === 0)
+        stmts.push('');
+      const library = value[name];
+      stmts.push(`import ${this.braces[0]}${name}${this.braces[1]} from ${this.quote}${library}${this.quote}${this.semicolon}`);
+    });
+    return stmts;
+  }
+
+  private produceNamespace(): string[] {
+    const stmts: string[] = [];
+    let imports:string[];
+    const value:Dictionary = this.namespaceImports;
+    if(this.sortImports) {
+      imports = this.sortedNamesIn(value);
+    } else {
+      imports = this.unsortedNamesIn(value);
+    }
+    imports.forEach((name, ix) => {
+      if (ix === 0)
+        stmts.push('');
+      const library = value[name];
+      stmts.push(`import * as ${name} from ${this.quote}${library}${this.quote}${this.semicolon}`);
+    });
+    return stmts;
+  }
+
+  private produceString(): string[] {
+    const stmts: string[] = [];
+    let strImports:string[] = this.stringImports.slice(0);
+    if(this.sortImports) {
+      strImports = strImports.sort(this.sortCaseInsensitive);
+    }
+    strImports.forEach((library, ix) => {
+      if (ix === 0)
+        stmts.push('');
+      stmts.push(`import ${this.quote}${library}${this.quote}${this.semicolon}`);
+    });
+    return stmts;
+  }
+
+  private sortCaseInsensitive(a, b): number {
+    return a.toLowerCase().localeCompare(b.toLowerCase());
+  }
+
+  private sortedNamesIn(dict: Dictionary): string[] {
+    return Object.keys(dict).sort(this.sortCaseInsensitive);
+  }
+
+  private unsortedNamesIn(dict:Dictionary):string[] {
+    return Object.keys(dict);
+  }
+  // private possibleSortedNamesIn(dict:Dictionary, toSort:boolean):string[] {
+  //   if(toSort) {
+  //     return this.sortedNamesIn(dict);
+  //   } else {
+  //     return this.unsortedNamesIn(dict);
+  //   }
+  // }
+
+  
+}
+
+/**
+ * Dictionary of imports
+ * 
+ * name of import ==> module ID
+ */
+
+ export interface Dictionary {
+   [nm: string]: string;
+ }
