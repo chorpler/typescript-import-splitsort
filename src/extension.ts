85d5f1c520028f8e2215c2d471280ab7bc2dfa63
David Sargeant <chorpler@gmail.com>
2018-12-16 11:19:40 -0600

    Some udpates for selections

--- src/extension.ts
+++ src/extension.ts
@@ -35,7 +35,7 @@ import { ExtensionContext } from 'vscode';
 import { Parser } from './parser';
 import { TextDocumentWillSaveEvent } from 'vscode';
 import { TextEdit } from 'vscode';
-import { TextEditorEdit } from 'vscode';
+// import { TextEditorEdit } from 'vscode';
 import { Selection } from 'vscode';
 import { Range } from 'vscode';
 import { Position } from 'vscode';
@@ -45,6 +45,8 @@ import { commands } from 'vscode';
 import { window } from 'vscode';
 import { workspace } from 'vscode';
 
+const emph:string = "color: black; font-weight: bold; background-color: rgba(255,128,128,0.5);";
+global['vscode'] = vscode;
 /**
  * import-splitnsort contract
  */
@@ -54,6 +56,8 @@ export function activate(context:ExtensionContext) {
   const disposable2 = commands.registerCommand('extension.typescript-import-splitsort.splitonly', splitAndDontSort);
   context.subscriptions.push(disposable1);
   context.subscriptions.push(disposable2);
+  console.log(`%cTypeScriptImportSplitSort activated`, emph);
+  global['tssscontext'] = context;
   onSave();
   workspace.onDidChangeConfiguration(() => onSave());
 }
@@ -65,17 +69,20 @@ export function deactivate() { }
 // let subscription: Nullable<Disposable>;
 let subscription:Disposable;
 let selection:Selection;
+let selections:Selection[];
 // let text:string;
 
+// let pos1:Position = new Position(0, 0);
+// pos1.
+// vscode.Selection.l
 function onSave() {
   // typescript-import-splitsort.leave-unsorted
   const config = workspace.getConfiguration('typescript-import-splitsort');
   if(config.get<boolean>('on-save')) {
     if(!subscription) {
-        subscription = workspace.onWillSaveTextDocument(splitAndSortOnSave);
+      subscription = workspace.onWillSaveTextDocument(splitAndSortOnSave);
     }
-  }
-  else {
+  } else {
     if(subscription) {
       subscription.dispose();
       subscription = null;
@@ -86,23 +93,32 @@ function onSave() {
 // @see https://github.com/mflorence99/import-splitnsort/issues/2
 
 function splitAndSort() {
-  console.log(`splitAndSort(): Called ...`);
+  console.log(`%c splitAndSort(): Called ...`, emph);
   const editor = window.activeTextEditor;
   if(!editor) {
-    console.log(`%c splitAndSort(): Could not get active editor.`, "color: black; font-weight: bold; background-color: rgba(255,128,128,0.5);");
+    console.log(`%c splitAndSort(): Could not get active editor.`, emph);
     return;
   }
-  let sortIt = true;
-  let line:TextLine;
+  let sortIt:boolean = false;
+  // let line:TextLine;
   let lineNumber:number;
+  let whatToReplace:Selection;
   let text:string;
   if(editor.selections && editor.selections.length) {
+    selections = editor.selections;
     selection = editor.selection;
   } else {
+    selections = editor.selections;
     selection = null;
   }
-  console.log(`splitAndSort(): Selection is:\n`, selection);
+  console.log(`splitAndSort(): Selections is:\n`, selections);
+  if(!selection) {
+    return;
+  }
   lineNumber = selection.active.line;
+  // console.log(`splitAndSort(): Selections, editor, window:\n`, selections);
+  // console.log(editor);
+  // console.log(window);
 
   // const config = workspace.getConfiguration('typescript-import-splitsort');
   // if(config.get<boolean>('leave-unsorted')) {
@@ -110,90 +126,257 @@ function splitAndSort() {
   // }
   let langId:string = editor && editor.document && typeof editor.document.languageId === 'string' ? editor.document.languageId : "UNKNOWN";
   if(langId === 'typescript') {
+    // let ln1:number = selection.start.line;
+    // let ln2:number = selection.end.line + 1;
+    // let pos1:Position = new Position(ln1, 0);
+    // let pos2:Position = new Position(ln2, 0);
+    let ln1:number = selection.start.line;
+    let ln2:number = selection.end.line;
+    let col1:number = selection.active.character;
+    let col2:number = selection.end.character;
+    let singleLine:boolean = (ln1 === ln2) || (ln2 === ln1 + 1 && col1 === 0) ? true : false;
+    // let line1:TextLine, line2:TextLine, rangeLine1:Range, rangeLine2:Range;
+    let line1:TextLine = editor.document.lineAt(ln1);
+    let line2:TextLine = editor.document.lineAt(ln2);
+    let rangeLine1:Range = line1.rangeIncludingLineBreak;
+    let rangeLine2:Range = line2.rangeIncludingLineBreak;
+    let range:Range;
+    if(singleLine) {
+      console.log(`RANGE WILL BE 1`);
+      range = line1.range;
+      // // line1 = editor.document.lineAt(ln1);
+      // range = rangeLine1;
+      // // text = line1.text;
+    } else if(col2 === 0) {
+      console.log(`RANGE WILL BE 2`);
+      // line1 = editor.document.lineAt(ln1);
+      line2 = editor.document.lineAt(ln2 - 1);
+      rangeLine2 = line2.range;
+      // rangeLine2 = line2.rangeIncludingLineBreak;
+      // let endChar:number = line2.text.length;
+      // let pos1:Position = new Position(ln1, 0);
+      // let pos2:Position = new Position(ln2-1, endChar);
+      // let range:Range = new Range(pos1, pos2);
+      range = rangeLine1.union(rangeLine2);
+      // text = editor.document.getText(range);
+    } else {
+      console.log(`RANGE WILL BE 3`);
+      range = rangeLine1.union(rangeLine2);
+      // let pos1:Position = new Position(ln1, 0);
+      // let pos2:Position = new Position(ln2+1, 0);
+      // let range:Range = new Range(pos1, pos2);
+      // text = editor.document.getText(range);
+      // let line1:TextLine = editor.document.lineAt(ln1);
+      // selection.start.
+    }
+    // let rangeLine1:Range = new Range(line1.range.start, line1.range.end);
+    // let rangeLine2:Range = new Range(line2.range.start, line2.range.end);
+    // let rangeLine2:Range = new Range(pos1, pos2);
+    // let range:Range;
+    // if(selection.isSingleLine) {
+    //   range = line2.range;
+    // } else {
+    //   range = rangeLine1.union(rangeLine2);
+    // }
+    text = editor.document.getText(range);
+    whatToReplace = new Selection(range.start, range.end);
+    console.log(`splitAndSort(): Range and whatToReplace:\n`, range);
+    console.log(whatToReplace);
+
+    // if(selection.isSingleLine) {
+    //   let line:TextLine = editor.document.lineAt(lineNumber);
+    //   text = line.text;
+    // } else {
+    //   text = editor.document.getText(range);
+    // }
+    // whatToReplace = new Selection(pos1, pos2);
+      // let line1:TextLine = editor.document.lineAt(ln1);
+      // selection.start.
+    // }
     // if(selection) {
     //   text = editor.document.getText(selection);
     // } else {
     //   text = editor.document.getText();
-      
     // }
-    line = editor.document.lineAt(lineNumber);
-    text = line.text;
-    console.log(`splitAndDontSort(): Text is:\n`, text);
-    Parser.makeEdits(text, sortIt).then((edits:TextEdit[]) => {
-        if(edits.length > 0) {
-          const range = edits[0].range;
-          const imports = edits[0].newText;
-          editor.edit(edit => edit.replace(range, imports));
-        }
-      });
+    // line = editor.document.lineAt(lineNumber);
+    // let lineSelection:Range = line.rangeIncludingLineBreak;
+    // let start:Position = lineSelection.start;
+    // let end:Position = lineSelection.end;
+    // let sel:Selection = new vscode.Selection(start, end);
+
+    // text = line.text;
+    console.log(`splitAndSort(): Text is:\n`, text);
+    if(!text.includes('import ')) {
+      console.log(`splitAndSort(): text does not include an import statement.`);
+      return;
+    }
+    Parser.makeEdits(text, sortIt, editor).then((edits:TextEdit[]) => {
+      console.log(`%c splitAndSort(): TextEdits are:`, emph);
+      console.log(edits);
+      if(edits.length > 0) {
+        // const range = edits[0].range;
+        const imports = edits[0].newText;
+        // editor.edit(edit => edit.replace(range, imports));
+        // editor.edit(edit => edit.replace(originalSelection, imports));
+        editor.edit(edit => edit.replace(whatToReplace, imports));
+      }
+    });
   } else {
     console.log(`splitAndSort(): Not running on document with syntax '${langId}'!`);
   }
 }
 
 function splitAndDontSort() {
-  console.log(`splitAndDontSort(): Called ...`);
+  console.log(`%c splitAndDontSort(): Called ...`, emph);
   const editor = window.activeTextEditor;
+  global['tssseditor'] = editor;
   if(!editor) {
-    console.log(`%c splitAndDontSort(): Could not get active editor.`, "color: black; font-weight: bold; background-color: rgba(255,128,128,0.5);");
+    console.log(`%c splitAndDontSort(): Could not get active editor.`, emph);
     return;
   }
-  let sortIt = false;
+  let sortIt:boolean = false;
+  // let originalSelection:Selection;
+  let whatToReplace:Selection;
   let text:string;
-  let line:TextLine;
-  let lineNumber:number;
+  // let line:TextLine;
+  // let lineNumber:number;
   // const config = workspace.getConfiguration('typescript-import-splitsort');
   // if(config.get<boolean>('leave-unsorted')) {
   //   sortIt = false;
   // }
-  if(editor.selection) {
-  // if(editor.selections && editor.selections.length) {
+  if(editor.selections && editor.selections.length) {
+    selections = editor.selections;
     selection = editor.selection;
   } else {
+    selections = editor.selections;
     selection = null;
   }
-  lineNumber = selection.active.line;
-  console.log(`splitAndDontSort(): Selection is:\n`, selection);
+  if(!selection) {
+    return;
+  }
+  // lineNumber = selection.active.line;
+
+  // originalSelection = new Selection(selection.anchor, selection.active);
+  // whatToReplace = new Selection(selection.anchor, selection.active)
+  // let replaceRange:Range = new Range(selection.anchor, selection.active);
+  // console.log(`splitAndDontSort(): Selections, editor, window:\n`, selections);
+  // console.log(editor);
+  // console.log(window);
   let langId:string = editor && editor.document && typeof editor.document.languageId === 'string' ? editor.document.languageId : "UNKNOWN";
   if(langId === 'typescript') {
+    // let ln1:number = selection.start.line;
+    // let ln2:number = selection.end.line + 1;
+    // let pos1:Position = new Position(ln1, 0);
+    // let pos2:Position = new Position(ln2, 0);
+    let ln1:number = selection.start.line;
+    let ln2:number = selection.end.line;
+    let col1:number = selection.active.character;
+    let col2:number = selection.end.character;
+    let singleLine:boolean = (ln1 === ln2) || (ln2 === ln1 + 1 && col1 === 0) ? true : false;
+    // let line1:TextLine, line2:TextLine, rangeLine1:Range, rangeLine2:Range;
+    let line1:TextLine = editor.document.lineAt(ln1);
+    let line2:TextLine = editor.document.lineAt(ln2);
+    let rangeLine1:Range = line1.rangeIncludingLineBreak;
+    let rangeLine2:Range = line2.rangeIncludingLineBreak;
+    let range:Range;
+    if(singleLine) {
+      console.log(`RANGE WILL BE 1`);
+      range = line1.range;
+      // // line1 = editor.document.lineAt(ln1);
+      // range = rangeLine1;
+      // // text = line1.text;
+    } else if(col2 === 0) {
+      console.log(`RANGE WILL BE 2`);
+      // line1 = editor.document.lineAt(ln1);
+      line2 = editor.document.lineAt(ln2 - 1);
+      rangeLine2 = line2.range;
+      // rangeLine2 = line2.rangeIncludingLineBreak;
+      // let endChar:number = line2.text.length;
+      // let pos1:Position = new Position(ln1, 0);
+      // let pos2:Position = new Position(ln2-1, endChar);
+      // let range:Range = new Range(pos1, pos2);
+      range = rangeLine1.union(rangeLine2);
+      // text = editor.document.getText(range);
+    } else {
+      console.log(`RANGE WILL BE 3`);
+      range = rangeLine1.union(rangeLine2);
+      // let pos1:Position = new Position(ln1, 0);
+      // let pos2:Position = new Position(ln2+1, 0);
+      // let range:Range = new Range(pos1, pos2);
+      // text = editor.document.getText(range);
+      // let line1:TextLine = editor.document.lineAt(ln1);
+      // selection.start.
+    }
+    // let rangeLine1:Range = new Range(line1.range.start, line1.range.end);
+    // let rangeLine2:Range = new Range(line2.range.start, line2.range.end);
+    // let rangeLine2:Range = new Range(pos1, pos2);
+    // let range:Range;
+    // if(selection.isSingleLine) {
+    //   range = line2.range;
+    // } else {
+    //   range = rangeLine1.union(rangeLine2);
+    // }
+    text = editor.document.getText(range);
+    whatToReplace = new Selection(range.start, range.end);
+    console.log(`splitAndDontSort(): Range and whatToReplace:\n`, range);
+    console.log(whatToReplace);
+
+    // if(selection.isSingleLine) {
+    //   let line:TextLine = editor.document.lineAt(lineNumber);
+    //   text = line.text;
+    // } else {
+    //   text = editor.document.getText(range);
+    // }
+    // whatToReplace = new Selection(pos1, pos2);
+      // let line1:TextLine = editor.document.lineAt(ln1);
+      // selection.start.
+    // }
     // if(selection) {
     //   text = editor.document.getText(selection);
     // } else {
     //   text = editor.document.getText();
     // }
-    line = editor.document.lineAt(lineNumber);
-    let lineSelection:Range = line.rangeIncludingLineBreak;
-    let start:Position = lineSelection.start;
-    let end:Position = lineSelection.end;
-    let sel:Selection = new vscode.Selection(start, end);
-    
-    text = line.text;
+    // line = editor.document.lineAt(lineNumber);
+    // let lineSelection:Range = line.rangeIncludingLineBreak;
+    // let start:Position = lineSelection.start;
+    // let end:Position = lineSelection.end;
+    // let sel:Selection = new vscode.Selection(start, end);
+
+    // text = line.text;
     console.log(`splitAndDontSort(): Text is:\n`, text);
-    Parser.makeEdits(text, sortIt).then((edits:TextEdit[]) => {
-        if(edits.length > 0) {
-          const range = edits[0].range;
-          const imports = edits[0].newText;
-          editor.edit(edit => edit.replace(range, imports));
-        }
-      });
-    } else {
-      console.log(`splitAndDontSort(): Not running on document with syntax '${langId}'!`);
+    if(!text.includes('import ')) {
+      console.log(`splitAndDontSort(): text does not include an import statement.`);
+      return;
     }
+    Parser.makeEdits(text, sortIt, editor).then((edits:TextEdit[]) => {
+      console.log(`%c splitAndDontSort(): TextEdits are:`, emph);
+      console.log(edits);
+      if(edits.length > 0) {
+        // const range = edits[0].range;
+        const imports = edits[0].newText;
+        // editor.edit(edit => edit.replace(range, imports));
+        // editor.edit(edit => edit.replace(originalSelection, imports));
+        editor.edit(edit => edit.replace(whatToReplace, imports));
+      }
+    });
+  } else {
+    console.log(`splitAndDontSort(): Not running on document with syntax '${langId}'!`);
   }
+}
 
 function splitAndSortOnSave(event: TextDocumentWillSaveEvent) {
-  // const editor = window.activeTextEditor;
-  let sortIt = true;
+  const editor = window.activeTextEditor;
+  let sortIt:boolean = true;
   const config = workspace.getConfiguration('typescript-import-splitsort');
   if(config.get<boolean>('leave-unsorted')) {
     sortIt = false;
   }
-  
+
   // event.document
   // let doc:TextDocument = event.document;
   let langId:string = event && event.document && typeof event.document.languageId === 'string' ? event.document.languageId : "UNKNOWN";
   if(langId === 'typescript') {
     let text:string = event.document.getText();
-    event.waitUntil(Parser.makeEdits(text, sortIt));
+    event.waitUntil(Parser.makeEdits(text, sortIt, editor));
   }
 }
--- src/parser.ts
+++ src/parser.ts
@@ -5,9 +5,11 @@ import { NamedImport } from 'typescript-parser';
 import { NamespaceImport } from 'typescript-parser';
 import { Position } from 'vscode';
 import { Range } from 'vscode';
+// import { window } from 'vscode';
 import { StringImport } from 'typescript-parser';
 import { SymbolSpecifier } from 'typescript-parser';
 import { TextEdit } from 'vscode';
+// import { TextEditor } from 'vscode';
 import { TypescriptParser } from 'typescript-parser';
 
 // const clone:Function = (val:any):any => {
@@ -15,6 +17,9 @@ import { TypescriptParser } from 'typescript-parser';
 //   let newVal = JSON.parse(tmpVal);
 //   return newVal;
 // };
+
+export type ImportsList = Map<string,string[]>;
+export type EditorImportsList = Map<string,ImportsList>;
 
 /**
  * Parse TypeScript and extract imports
@@ -37,25 +42,60 @@ export class Parser {
 
   private parser = new TypescriptParser();
 
-  public static sortImports = false;
+  public static lastParser:Parser;
 
+  public static editorImportsList:EditorImportsList = new Map();
+  public get editorImportsList():EditorImportsList { return Parser.editorImportsList; }
+  public set editorImportsList(val:EditorImportsList) { Parser.editorImportsList = val; }
+
+  public static importList:ImportsList = new Map();
+  public get importList():ImportsList { return Parser.importList; }
+  public set importList(val:ImportsList) { Parser.importList = val; }
+  // public importList:ImportsList = new Map();
+
+  public static sortImports:boolean = false;
   public get sortImports():boolean { return Parser.sortImports; }
   public set sortImports(val:boolean) { Parser.sortImports = Boolean(val); }
 
   /** ctor */
-  constructor(private src: string) {
+  // constructor(private src: string, vsceditor:TextEditor) {
+  constructor(private src: string, vsceditor:any) {
+    Parser.lastParser = this;
     console.log(`Parser constructed with source:\n`, src);
+    // console.log(`Static Parser is:\n`, Parser);
+    // console.log(`This parser is:\n`, this);
+    global['tsssparser'] = this;
+    global['tssseditor'] = vsceditor;
+    // let editor = window.activeTextEditor.
+    if(vsceditor != undefined) {
+      let id:string = vsceditor._id;
+      let eil:EditorImportsList = Parser.editorImportsList;
+      let importsList:ImportsList;
+      if(eil.has(id)) {
+        importsList = eil.get(id);
+      } else {
+        importsList = new Map();
+        Parser.importList = importsList;
+        eil.set(id, importsList);
+      }
+      global['tssseditorimportlist'] = eil;
+    }
   }
 
   /** Make an edit to replace imports */
-  static makeEdits(src: string, toSort:boolean): Promise<TextEdit[]> {
+  static makeEdits(src: string, toSort:boolean, vsceditor:any): Promise<TextEdit[]> {
     return new Promise((resolve, reject) => {
       if(toSort) {
         Parser.sortImports = true;
       }
-      const parser = new Parser(src);
+      const parser = new Parser(src, vsceditor);
       parser.parse().then(() => {
         const imports = parser.produce();
+        // console.log(`Parser.makeEdits(): importsList is now:\n`, Parser.importList);
+        // let strList:string = JSON.stringify(Parser.importList);
+        // console.log(`Parser.makeEdits(): importsList is now:\n`, strList);
+        global['tsssimportlist'] = Parser.importList;
+        global['tssseditorimportlist'] = Parser.editorImportsList;
         resolve((imports.length > 0)? [TextEdit.replace(parser.range, imports)] : []);
       });
     });
@@ -71,6 +111,14 @@ export class Parser {
           let start = Number.MAX_SAFE_INTEGER;
           let end = Number.MIN_SAFE_INTEGER;
           file.imports.forEach((node: Import) => {
+            // let libName:string = node.libraryName;
+            // let importName:string = node.
+            // let libImports:string[];
+            // if(this.importList.has(libName)) {
+            //    libImports = this.importList.get(libName);
+            // } else {
+            //   libImports = [];
+            // }
             start = Math.min(start, node.start);
             end = Math.max(end, node.end);
             // process each type of import
@@ -106,70 +154,90 @@ export class Parser {
       .concat(this.produceExternal())
       .concat(this.produceString());
     // NOTE: if there are any imports at all, there'll be an initial blank line
+    // return (stmts.length > 0)? stmts.slice(1).join('\n') : '';
     return (stmts.length > 0)? stmts.slice(1).join('\n') : '';
   }
 
   // private methods
 
+  public addImportToLibrary(libraryName:string, importAlias:string):string[] {
+    let li:ImportsList = Parser.importList;
+    let imports:string[];
+    // console.log(`addImportsToLibrary(): Attempting to add '${importAlias}' to library '${libraryName}'`);
+    if(li.has(libraryName)) {
+      imports = li.get(libraryName);
+    } else {
+      imports = [];
+      li.set(libraryName, imports);
+    }
+    imports.push(importAlias);
+    return imports;
+  }
+
   private detectCodingStyle(start: number,
                             end: number): void {
     const imports = this.src.substring(start, end);
-    if (!imports.includes(this.braces[0])) {
+    if(!imports.includes(this.braces[0])) {
       this.braces[0] = '}';
     }
-    if (!imports.includes(this.braces[1])) {
+    if(!imports.includes(this.braces[1])) {
       this.braces[1] = '}';
     }
-    if (!imports.includes(this.quote)) {
+    if(!imports.includes(this.quote)) {
       this.quote = '"';
     }
-    if (!imports.includes(this.semicolon)) {
+    if(!imports.includes(this.semicolon)) {
       this.semicolon = '';
     }
   }
 
   private extractExternal(node: ExternalModuleImport): void {
     this.externalImports[node.alias] = node.libraryName;
+    this.addImportToLibrary(node.libraryName, node.alias);
   }
 
   private extractNamed(node: NamedImport): void {
-    if (node.defaultAlias) {
+    if(node.defaultAlias) {
       this.defaultImports[node.defaultAlias] = node.libraryName;
+      this.addImportToLibrary(node.libraryName, node.defaultAlias);
     }
     node.specifiers.forEach((specifier: SymbolSpecifier) => {
       const initialChar = specifier.specifier[0];
-      const dict = (initialChar === initialChar.toLowerCase())?
-        this.namedFunctionImports : this.namedClassImports;
-      if (specifier.alias) {
-        dict[`${specifier.specifier} as ${specifier.alias}`] = node.libraryName;
+      const dict = (initialChar === initialChar.toLowerCase()) ? this.namedFunctionImports : this.namedClassImports;
+      let importName:string;
+      if(specifier.alias) {
+        importName = `${specifier.specifier} as ${specifier.alias}`;
+      } else {
+        importName = specifier.specifier;
+        // dict[specifier.specifier] = node.libraryName;
       }
-      else { dict[specifier.specifier] = node.libraryName; }
+      dict[importName] = node.libraryName;
+      this.addImportToLibrary(node.libraryName, importName);
     });
   }
 
   private extractNamespace(node: NamespaceImport): void {
     this.namespaceImports[node.alias] = node.libraryName;
+    this.addImportToLibrary(node.libraryName, node.alias);
   }
 
-  private extractString(node: StringImport): void {
+  private extractString(node:StringImport): void {
     this.stringImports.push(node.libraryName);
   }
 
   // NOTE: awkward! we have to convert characters from parser into line/character
-  private makeRange(start: number,
-                    end: number): void {
-    let line = 0, character = 0;
+  private makeRange(start:number, end:number): void {
+    let line:number = 0, character:number = 0;
     let spos: Position, epos: Position;
-    for (let ix = 0; ix < end; ix++) {
-      if (ix === start) {
+    for(let ix = 0; ix < end; ix++) {
+      if(ix === start) {
         spos = new Position(line, character);
       }
       // increment counters
-      if (this.src[ix] === '\n') {
+      if(this.src[ix] === '\n') {
         line += 1;
         character = 0;
-      }
-      else { character += 1; }
+      } else { character += 1; }
     }
     // complete range
     epos = new Position(line, character);
@@ -186,7 +254,7 @@ export class Parser {
       imports = this.unsortedNamesIn(value);
     }
     imports.forEach((name, ix) => {
-      if (ix === 0) {
+      if(ix === 0) {
         stmts.push('');
       }
       const library = value[name];
@@ -195,7 +263,7 @@ export class Parser {
     return stmts;
   }
 
-  private produceExternal(): string[] {
+  private produceExternal():string[] {
     const stmts: string[] = [];
     let imports:string[];
     const value:Dictionary = this.externalImports;
@@ -205,7 +273,7 @@ export class Parser {
       imports = this.unsortedNamesIn(value);
     }
     imports.forEach((name, ix) => {
-      if (ix === 0) {
+      if(ix === 0) {
         stmts.push('');
       }
       const library = value[name];
@@ -224,7 +292,7 @@ export class Parser {
       imports = this.unsortedNamesIn(value);
     }
     imports.forEach((name, ix) => {
-      if (ix === 0) {
+      if(ix === 0) {
         stmts.push('');
       }
       const library = value[name];
@@ -243,7 +311,7 @@ export class Parser {
       imports = this.unsortedNamesIn(value);
     }
     imports.forEach((name, ix) => {
-      if (ix === 0) {
+      if(ix === 0) {
         stmts.push('');
       }
       const library = value[name];
@@ -262,7 +330,7 @@ export class Parser {
       imports = this.unsortedNamesIn(value);
     }
     imports.forEach((name, ix) => {
-      if (ix === 0) {
+      if(ix === 0) {
         stmts.push('');
       }
       const library = value[name];
@@ -278,7 +346,7 @@ export class Parser {
       strImports = strImports.sort(this.sortCaseInsensitive);
     }
     strImports.forEach((library, ix) => {
-      if (ix === 0) {
+      if(ix === 0) {
         stmts.push('');
       }
       stmts.push(`import ${this.quote}${library}${this.quote}${this.semicolon}`);
@@ -305,12 +373,19 @@ export class Parser {
   //   }
   // }
 
-  
+  public getClass():any {
+    return Parser;
+  }
+
+  public getClassName():string {
+    return 'Parser';
+  }
+
 }
 
 /**
  * Dictionary of imports
- * 
+ *
  * name of import ==> module ID
  */
 
